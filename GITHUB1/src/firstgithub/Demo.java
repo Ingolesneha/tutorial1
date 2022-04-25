9f84bc9a11caa834f10581e76bb8e4072c1a2f16
Ingolesneha <104188015+Ingolesneha@users.noreply.github.com>
2022-04-25 12:05:30 +0530

    Branch1

--- GITHUB1/src/firstgithub/Demo.java
+++ GITHUB1/src/firstgithub/Demo.java
@@ -1,6 +1,10 @@
 package firstgithub;
 
 public class Demo {
+	public void m1()
+	{
+		System.out.println("M1 of Demo") ; 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
